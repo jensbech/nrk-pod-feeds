f5c0deda04e9ac881b9d71e2d4e1de6262ae39f2
Feed Updater <actions@github.com>
2026-09-17 07:37:23 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -1092,7 +1092,7 @@ const feeds = [
     "id": "radiodokumentaren",
     "title": "Historier fra virkeligheten",
     "season": "LATEST_SEASON",
-    "enabled": false,
+    "enabled": true,
     "image": "https://gfx.nrk.no/vHLmZEDQBRftGcSloLbN8Q5Y7GS29Ym38qScCBrSwoUA.jpg"
   },
   {
